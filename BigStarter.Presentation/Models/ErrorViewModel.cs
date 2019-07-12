4c1d1491b0e40395ba1683fdd66c64d0ea9f38aa
Shuhtov1 <shuhtov1@gmail.com>
2019-07-12 18:48:23 +0300

    Add project files.

---
+++ BigStarter.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BigStarter.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
