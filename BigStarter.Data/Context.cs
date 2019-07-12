3fdcdb7e66996ad45bd0393b304f8d4409e74ddc
Shuhtov1 <shuhtov1@gmail.com>
2019-07-12 19:58:42 +0300

    Add 2 project and set EFcore

---
+++ BigStarter.Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigStarter.Data
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Data Source=DESKTOP-A3BNG3V\SQLEXPRESS;Initial Catalog=BigStarterDb;Integrated Security=True");
+            }
+            
+        }
+    }
+}
