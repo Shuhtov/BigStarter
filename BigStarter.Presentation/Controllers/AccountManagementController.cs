2a41b2b163b3036b22a4dc0dc76d510a0b47e673
Shuhtov1 <shuhtov1@gmail.com>
2019-07-13 14:38:43 +0300

    add form login\register and any

---
+++ BigStarter.Presentation/Controllers/AccountManagementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BigStarter.Presentation.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace BigStarter.Presentation.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        List<UserSoc> userList = new List<UserSoc>();
+        private readonly IAuthenticationSchemeProvider authenticationSchemeProvider;
+        public AccountManagementController(IAuthenticationSchemeProvider authenticationSchemeProvider)
+        {
+            this.authenticationSchemeProvider = authenticationSchemeProvider;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SignInSocial(String provider)
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/Home/Index" }, provider);
+        }
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- BigStarter.Presentation/Startup.cs
+++ BigStarter.Presentation/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
@@ -9,9 +11,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BigStarter.Presentation.Data;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using BigStarter.Presentation.Data;
 
 namespace BigStarter.Presentation
 {
@@ -24,26 +29,50 @@ namespace BigStarter.Presentation
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString(@"DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+
+                .AddFacebook(facebookOptions =>
+                {
+                    facebookOptions.AppId = "1260978114083269";
+                    facebookOptions.AppSecret = "fcd6f386c93f599b12796f06537e8e5c";
+                })
+
+                .AddTwitter(twitterOptions =>
+                {
+                    twitterOptions.ConsumerKey = "hjOg61C46NNEb3v0YVWLAVdkh";
+                    twitterOptions.ConsumerSecret = "JCbfT2ZZQUJUvayqxPFbW5E5FZMgJvuzAmJcMcKHDXz54XjiGi";
+                })
+                .AddGoogle(googleOptions =>
+                {
+                    googleOptions.ClientId = "336661259970-nh2e7ijk8qssm958e58vjemoid4cgdi9.apps.googleusercontent.com";
+                    googleOptions.ClientSecret = "ejvyjYjeE6CKVf9t3XQhYzMD";
+                })
+
+                .AddCookie();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
